13f5d2f6dfa71b63877643a4ada2884220671784
saebom <boma91@gmail.com>
2021-02-22 10:12:57 +0900

    Add: js기능 구현 완료

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const form = document.querySelector("#form");
-const username = document.getElementById("username");
+const username = document.getElementById("userName");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
-const small = document.querySelector("small");
+//const small = document.querySelector("small");
 
 // Show input "error message"
 function showError(input, message) {
     console.log(input.parentElement);
     const formControl = input.parentElement;
-
     formControl.className = "form-control error";
+
+    const small = formControl.querySelector("small"); // 지역변수로 선언해줘야 한다
     small.innerText = message;
 }
 
@@ -22,7 +23,10 @@ function showSuccess(input) {
     formControl.className = "form-control success";
 }
 
-/////////////////////
+// 입력받은 값의 id이름(username, password, email 등)의 앞글자를 대문자로 바꿔서 출력
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 // Check email is valid
 function checkEmail(input) {
@@ -33,37 +37,32 @@ function checkEmail(input) {
     }
 }
 
-// Get fieldname
-// function getFieldName(input) {
-//     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-// }
-
 // Check input length
 function checkLength(input, min, max) {
     if (input.value.length < min) {
-        showError(input, `must be at least ${min} characters`);
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
     } else if (input.value.length > max) {
-        showError(input, `must be less than ${max} characters`);
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
     } else {
         showSuccess(input);
     }
 }
 
 // Check required fields
-// function checkRequired(inputArr) {
-//     let isRequired = false;
+function checkRequired(inputArr) {
+    let isRequired = false;
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === "") {
+            // 값이 없으면
+            showError(input, `${getFieldName(input)} is required`);
+            isRequired = true;
+        } else {
+            showSuccess(input);
+        }
+    });
 
-//     inputArr.forEach(function (input) {
-//         if (input.indexOf() !== -1) {
-//             showError(input, `${getFieldName(input)} is required`);
-//             isRequired = true;
-//         } else {
-//             showSuccess(input);
-//         }
-//     });
-
-//     return isRequired;
-// }
+    return isRequired;
+}
 
 // Check passwords match
 function checkPasswordsMatch(input1, input2) {
@@ -75,10 +74,12 @@ function checkPasswordsMatch(input1, input2) {
 // Event listeners
 form.addEventListener("submit", function (e) {
     // console.log(e.target);
-    // e.preventDefault();
+    e.preventDefault();
 
-    checkLength(username, 3, 10);
-    checkLength(password, 6, 25);
-    checkEmail(input);
-    checkPasswordsMatch(password, password2);
+    if (!checkRequired([username, email, password, password2])) {
+        checkLength(username, 3, 15);
+        checkLength(password, 6, 25);
+        checkEmail(email);
+        checkPasswordsMatch(password, password2);
+    }
 });
